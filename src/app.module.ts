fb699649026b328eb6d97d5a1119274bf4406d18
Jakub Stibůrek <alcolp96@gmail.com>
2022-04-18 10:12:42 +0200

    Implement websockets gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChatGateway } from './chat.gateway';
 import secrets from './secrets';
 
 @Module({
   imports: [MongooseModule.forRoot(secrets.mongodb)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
